65d3e6cd31041c3554fac8e6377299c0fcd43eb7
Carlos Samir Diaz Gaviria <109492335+elsammio@users.noreply.github.com>
2025-11-23 15:57:57 -0500

    Creacion clase Arena

---
+++ Lyrenhold/src/Arena.cpp
@@ -0,0 +1,5 @@
+//
+// Created by CRASHO on 23/11/2025.
+//
+
+#include "Arena.h"
---
+++ Lyrenhold/src/Arena.h
@@ -0,0 +1,13 @@
+//
+// Created by CRASHO on 23/11/2025.
+//
+
+#ifndef LYRENHOLD_ARENA_H
+#define LYRENHOLD_ARENA_H
+
+
+class Arena {
+};
+
+
+#endif //LYRENHOLD_ARENA_H
