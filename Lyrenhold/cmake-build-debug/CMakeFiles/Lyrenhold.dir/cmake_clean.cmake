aa020f467d3c5a27324c59f247ad7867be503e27
luis <luisalejandropcoral@gmail.com>
2025-11-23 15:39:03 -0500

    Creación de la carpeta src

---
+++ Lyrenhold/cmake-build-debug/CMakeFiles/Lyrenhold.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lyrenhold.dir/main.cpp.obj"
+  "CMakeFiles/Lyrenhold.dir/main.cpp.obj.d"
+  "Lyrenhold.exe"
+  "Lyrenhold.exe.manifest"
+  "Lyrenhold.pdb"
+  "libLyrenhold.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lyrenhold.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Lyrenhold/cmake-build-debug/CMakeFiles/Lyrenhold.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Lyrenhold.
+# This may be replaced when dependencies are built.
